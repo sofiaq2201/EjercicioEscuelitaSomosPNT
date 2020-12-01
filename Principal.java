da7a62a2923e60b1139b797a8da5d70048aa8001
Sofia Quispe <38408353+sofiaq2201@users.noreply.github.com>
2020-12-01 14:14:16 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Principal {
 
     public static void main(String[] args) {
-       Producto p = new Producto();
+       List<Producto> productos = new ArrayList<>();
        
-       p.agregarProducto(new Bebida("Coca-Cola Zero", 20, 1.5));
-       p.agregarProducto(new Bebida("Coca-Cola", 18, 1.5));
-       p.agregarProducto(new Shampoo("Shampoo Sedal", 19, 500));
-       p.agregarProducto(new Verdura("Frutillas", 64, "kilo"));
+       productos.add(new Bebida("Coca-Cola Zero", 20, 1.5));
+       productos.add(new Bebida("Coca-Cola", 18, 1.5));
+       productos.add(new Shampoo("Shampoo Sedal", 19, 500));
+       productos.add(new Verdura("Frutillas", 64, "kilo"));
        
-       p.Menu();
-    }
+       for(Producto producto: productos){
+          System.out.println(producto.toString());
+       }
+       
+       Collections.sort(productos);
+       String productoMasCaro = productos.get(productos.size() - 1).getNombre();
+       String productoMasBarato =  productos.get(0).getNombre();
+       
+       System.out.println("=============================");
+       System.out.println("Producto más caro: " + productoMasCaro);
+       System.out.println("Producto más barato: " + productoMasBarato);
     
+    } 
 }
